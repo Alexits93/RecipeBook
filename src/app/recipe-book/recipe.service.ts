a584f72e7921971f1e13fad6ed0a9652de279a84
Alexits Patrik <alexitspatrik@gmail.com>
2019-06-27 15:55:48 +0200

    adding the selected recipe's ingredients to the shopping list

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,16 +1,23 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Hungarian Gulasch', 'From Hungary', 'http://www.mnhsz.com/storage/upload/2017/marhaporkolt_bogracsban.jpg'),
-    new Recipe('Angus Steak', 'From the USA', 'https://images-na.ssl-images-amazon.com/images/I/51NYBtXasUL.jpg'),
-    new Recipe('Jalapeno Cheese Soup', 'From Mexico', 'http://34.198.236.63/wp-content/uploads/2014/12/Jalapeno-Cheddar-Soup.jpg')
+    new Recipe('Hungarian Gulasch', 'From Hungary', 'http://www.mnhsz.com/storage/upload/2017/marhaporkolt_bogracsban.jpg', [
+      new Ingredient('Meat', 1),
+      new Ingredient('Paprika', 5)
+    ]),
+    new Recipe('Angus Steak', 'From the USA', 'https://images-na.ssl-images-amazon.com/images/I/51NYBtXasUL.jpg', [
+      new Ingredient('Beef', 3),
+      new Ingredient('Steak potato', 10)
+    ]),
+    new Recipe('Jalapeno Cheese Soup', 'From Mexico', 'http://34.198.236.63/wp-content/uploads/2014/12/Jalapeno-Cheddar-Soup.jpg', [
+      new Ingredient('Cheese', 2),
+      new Ingredient('Jalapeno', 7)
+    ])
   ];
 
   getRecipes(): Recipe[] {
